6fa2f049c8309ee1d2d52f25293c08461e610c74
João Marques <jpm@advert.io>
2018-01-08 00:40:04 +0000

    add delete and check button

--- index.js
+++ index.js
@@ -2,27 +2,51 @@ const todoList = ['Fazer isto', 'Fazer aquilo', 'Acabar a coisa', 'Fazer isto', 
 
 document.addEventListener('DOMContentLoaded', () => {
   const list = document.getElementById("list");
-  buildListFromArray(todoList);
+  buildMenuFromList(list, todoList);
+  console.log(list)
+
 });
 
-const buildListFromArray = array => (
-  array.map(item => {
+const buildMenuFromList = (context, stringList) => (
+  stringList.map(item => {
     const listItem = createCustomElementLi();
-    const linkItem = createCustomElementLink(item);
-
-    list.appendChild(listItem);
-    listItem.appendChild(linkItem);
+    createCustomElementLink(listItem, item);
+    context.appendChild(listItem);
   })
 );
 
 const createCustomElementLi = () => (
-  document.createElement("LI")
+  document.createElement('LI')
 )
 
-const createCustomElementLink = (text) => {
-  const linkItem = document.createElement("a");
+const createCustomElementLink = (context, text) => {
+  const linkItem = document.createElement('a');
   linkItem.classList.add('small');
   linkItem.classList.add('macos-font-color');
   linkItem.textContent = text;
-  return linkItem;
+  createIconButton(linkItem, ['fa', 'fa-times'], deleteTodo);
+  createIconButton(linkItem, ['fa', 'fa-check'], toggleTodo);
+  context.appendChild(linkItem);
+}
+
+const createIconButton = (context, cssClassList, action) => {
+  const span = createSpan(cssClassList, action);
+  context.appendChild(span);
+}
+
+const createSpan = (cssClassList, action) => {
+  const span = document.createElement('SPAN');
+  span.onclick = action;
+  for (i of cssClassList) {
+    span.classList.add(i);
+  }
+  return span;
+}
+
+const deleteTodo = () => {
+  console.log('delete todo');
+}
+
+const toggleTodo = () => {
+  console.log('toggle todo');
 }
